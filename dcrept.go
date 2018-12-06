7e5c77c4cbe8396f9ca3400f8d70376518bb1119
Andreas Krennmair <ak@synflood.at>
2018-12-06 23:12:37 +0100

    and a bit more.

--- dcrept.go
+++ dcrept.go
@@ -1,5 +1,42 @@
 package main
 
+import "fmt"
+
 func dcrept(v int) {
-	// TODO: implement
+	var m1, m2 float64
+
+	/* set up the magic factors to output the time till fixed */
+	if ship.cond == DOCKED {
+		m1 = 1.0 / param.dockfac
+		m2 = 1.0
+	} else {
+		m1 = 1.0
+		m2 = param.dockfac
+	}
+	fmt.Printf("Damage control report:\n")
+	f := true
+
+	for i := 0; i < MAXEVENTS; i++ {
+		e := &eventList[i]
+		if e.evcode != E_FIXDV {
+			continue
+		}
+
+		/* output the title first time */
+		if f {
+			fmt.Printf("\t\t\t  repair times\n")
+			fmt.Printf("device\t\t\tin flight  docked\n")
+			f = false
+		}
+
+		/* compute time till fixed, then adjust by the magic factors */
+		x := e.date - now.date
+		fmt.Printf("%-24s%7.2f  %7.2f\n", devices[e.systemname].name, x*m1+0.005, x*m2+0.005)
+
+		/* do a little consistancy checking */
+	}
+
+	if f {
+		fmt.Printf("All device functional\n")
+	}
 }
--- dock.go
+++ dock.go
@@ -3,7 +3,60 @@ package main
 import "fmt"
 
 func dock(v int) {
-	// TODO: implement
+	if ship.cond == DOCKED {
+		fmt.Printf("Chekov: But captain, we are already docked\n")
+		return
+	}
+
+	/* check for ok to dock, i.e., adjacent to a starbase */
+	ok := false
+	for i := ship.sectx - 1; i <= ship.sectx+1 && !ok; i++ {
+		if i < 0 || i >= NSECTS {
+			continue
+		}
+		for j := ship.secty - 1; j <= ship.secty+1; j++ {
+			if j < 0 || j >= NSECTS {
+				continue
+			}
+			if sect[i][j] == BASE {
+				ok = true
+				break
+			}
+		}
+	}
+	if !ok {
+		fmt.Printf("Chekov: But captain, we are not adjacent to a starbase.\n")
+		return
+	}
+
+	/* restore resources */
+	ship.energy = param.energy
+	ship.torped = param.torped
+	ship.shield = param.shield
+	ship.crew = param.crew
+	game.captives += param.brigfree - ship.brigfree
+	ship.brigfree = param.brigfree
+
+	/* reset ship's defenses */
+	ship.shldup = false
+	ship.cloaked = false
+	ship.cond = DOCKED
+	ship.reserves = param.reserves
+
+	/* recalibrate space inertial navigation system */
+	ship.sinsbad = false
+
+	/* output any saved radio messages */
+	dumpssradio()
+
+	/* reschedule any device repairs */
+	for i := 0; i < MAXEVENTS; i++ {
+		e := &eventList[i]
+		if e.evcode != E_FIXDV {
+			continue
+		}
+		reschedule(e, (e.date-now.date)*param.dockfac)
+	}
 }
 
 func undock(_ int) {
